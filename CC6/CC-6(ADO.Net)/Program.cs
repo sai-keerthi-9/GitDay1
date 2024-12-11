ce2adbeeeb696655c7ca86717bc19c9862159f55
Saikeerthi Tummagunta <saikeerthit@infinite.com>
2024-12-11 11:58:47 +0530

    cc-6

---
+++ CC6/CC-6(ADO.Net)/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CC_6_ADO.Net_
+{
+    class Program
+    {
+        public static SqlConnection con=null;
+        public static SqlCommand cmd=null;
+
+        static void Main(string[] args)
+        {
+
+            using (SqlConnection con = new SqlConnection("Server=ICS-LT-D244D69Q;initial catalog=assessments; integrated security=true;"))
+            {
+                con.Open();
+                using (SqlCommand cmd = new SqlCommand("Product_Details", con))
+                {
+                    //input for procedure
+
+                    cmd.Parameters.Add(new SqlParameter("@pname", "p1"));
+                    cmd.Parameters.Add(new SqlParameter("@price", SqlDbType.Int)).Value = 50000;
+                    //output for procedure
+                    SqlParameter param1 = new SqlParameter("@pid", SqlDbType.Int);
+                    param1.Direction = ParameterDirection.Output;
+                    cmd.Parameters.Add(param1);
+                    SqlParameter param2 = new SqlParameter("@discount", SqlDbType.Float);
+                    param2.Direction = ParameterDirection.Output;
+                    cmd.Parameters.Add(param2);
+                    cmd.ExecuteNonQuery();
+                    int product_id = Convert.ToInt32(cmd.Parameters["@pid"].Value);
+                    int discount = Convert.ToInt32(cmd.Parameters["@discount"].Value);
+                    Console.WriteLine($"Product ID : {product_id}  ,  Discount Price : {discount}");
+
+                }
+            }
+        Console.Read();
+            }
+           
+        }
+    }
---
+++ CC6/SQL(cc-6).sql
@@ -0,0 +1,76 @@
+use assessments
+create table Course_Details
+(
+CID varchar(10),
+CName varchar(20),
+StartDate datetime,
+EndDate datetime,
+Fee int
+)
+
+insert into Course_Details values('DN003','DotNet','2018-02-01','2018-02-28',15000),
+('DV004','Data Visualization','2018-03-01','2018-04-15',15000),
+('JA002','Advanced Java','2018-01-02','2018-01-20',10000),
+('JC001','Core Java','2018-01-02','2018-01-12',3000)
+
+select * from Course_Details
+
+--1.Create a Function to calculate the course duration for the above relation by receiving start date and end date as input.
+
+create or alter function Course_Duration (@startdate datetime,@enddate datetime)
+returns int
+as
+begin
+declare @duration int 
+select @duration=DATEDIFF(DAY,Startdate,EndDate) from Course_Details
+return @duration
+end
+
+select CID,CName,assessments.dbo.Course_Duration(StartDate,EndDate) as Duration,Fee from Course_Details
+
+
+--2.Create a trigger to display the Course Name and Start date of the course
+
+create or alter trigger tgrDisplay
+on Course_Details
+for insert
+as
+begin
+ insert into Course_Details(CName,StartDate)
+ select CName,StartDate from inserted
+ select * from Course_Details
+end
+
+insert into Course_Details values('AB009','Devops','2018-07-09','2018-08-09',5000)
+
+--3.Write a stored Procedure that inserts records in the ProductsDetails table
+create table Products
+(
+PID int,
+PName varchar(20),
+Price int,
+DiscountPrice float
+)
+select * from Products
+
+--Procedure
+create or alter proc Product_Details(@pname varchar(20),@price int,@pid int out,@discount float out)
+as
+begin
+  set @discount=@price*0.10
+  declare @maxpid int 
+   select @maxpid=ISNULL(MAX(PID),0)+1 from Products
+   insert into Products values(@maxpid,@pname,@price,@discount)
+
+end
+
+declare @id int,@discount varchar(20),@pname varchar(20),@price int
+Exec Product_Details 'Keerthi', 10000, @id output ,@discount output
+select @pname=PName,@price=Price from Products
+print 'PID : '+cast(@id as varchar(10))+'Product Name : ' +cast(@pname as varchar(10))
+        +'Price : '+cast(@price as varchar(10))+'Discount : '+cast(@id as varchar(10))
+
+		select * from Products
+		
+		
+
