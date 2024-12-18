a9f66dfdfe058cdf04f01a1fa462a0385dd6ea38
Saikeerthi Tummagunta <saikeerthit@infinite.com>
2024-12-18 11:25:24 +0530

    cc-7

---
+++ CC7/CC-7(Web Technologies)/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CC_7_Web_Technologies_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
