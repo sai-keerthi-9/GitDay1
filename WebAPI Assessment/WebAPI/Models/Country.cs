a473451cc6b1f9c7eaf3940b7e3b6ffb70c31bdf
Saikeerthi Tummagunta <saikeerthit@infinite.com>
2025-01-08 10:51:05 +0530

    web-api assessment

---
+++ WebAPI Assessment/WebAPI/Models/Country.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI.Models
+{
+    public class Country
+    {
+        public int CountryId { get; set; }
+        public string CountryName { get; set; }
+        public string Capital { get; set; }
+    }
+}
